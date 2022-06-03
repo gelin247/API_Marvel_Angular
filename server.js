c2d009c708c72cf050c89e456695bf8d61e17852
Angelo <angeloglps@gmail.com>
2022-06-03 14:35:31 -0700

    serve.js update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<angular-api>'));
+app.use(express.static(__dirname + '/dist/angular-api'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/<angular-api > /index.html'));});
+    res.sendFile(path.join(__dirname + '/dist/angular-api/index.html'));});
 
 app.listen(process.env.PORT || 8080);
