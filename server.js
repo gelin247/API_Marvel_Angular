591ad3aba52d7650eb181e22f088aa96bd101c51
Angelo <angeloglps@gmail.com>
2022-06-03 14:53:11 -0700

    server.js update

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/angular-api'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/angular-api/index.html'));});
 
-app.listen(process.env.PORT || 8080);
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL;
+
+app.use(express.static(__dirname + '/dist/ng-marvel'));
+
+app.get('/xpto', (req, res) => {
+    res.send(API_KEY);
+});
+
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/ng-marvel/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(API_KEY);
+    console.log(API_URL);
+    console.log(`Server listening on port ${PORT}`);
+})
